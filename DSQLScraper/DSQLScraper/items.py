b2dc924ac37efc440d7007769186c949340af4cf
mlmsmith <57990458+mlmsmith@users.noreply.github.com>
2022-09-03 19:04:31 +0100

    item loader updated

--- DSQLScraper/DSQLScraper/items.py
+++ DSQLScraper/DSQLScraper/items.py
@@ -1,12 +1,33 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
+import attr
 import scrapy
+from scrapy.loader import ItemLoader
+from itemloaders.processors import TakeFirst, MapCompose
+from w3lib.html import remove_tags
+import re
+from reader import r
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import Rule
+
+
+def to_float(value):
+    return float(re.sub('[^\d\.]', '', value))
 
 
 class DsqlscraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+
+    def __init__(self):
+        super().__init__()
+
+        for i in range(len(r.categories)):
+            stipulations = []
+            if 'clean' in r.modifiers[i]: stipulations.append(remove_tags)
+            if 'float' in r.modifiers[i]: stipulations.append(to_float)
+            self.fields[r.categories[i]] = scrapy.Field(input_processor=MapCompose(*stipulations),
+                                                        output_processor=TakeFirst())
+
+
+rules2 = []
+d = {'allow': 'collections'}
+x = 'collections/japanese-whisky'
+
+# extractor = LinkExtractor.
